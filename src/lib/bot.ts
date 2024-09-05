38a86cc1a385e47177a615cf003837088722e079
Shahbaz Ansari <shahbaz4783@gmail.com>
2024-09-05 19:49:36 +0530

    delete bot file

--- src/lib/bot.ts
+++
@@ -1,9 +0,0 @@
-import { Bot } from 'grammy';
-
-const { BOT_TOKEN: token = '' } = process.env;
-
-export const bot = new Bot(token);
-
-bot.on('message', async (ctx) => {
-	await ctx.reply('Hi there! From Nextjs');
-});
