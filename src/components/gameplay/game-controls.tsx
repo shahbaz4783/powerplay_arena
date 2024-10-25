e328ee7582062d62584625c8691e6c4d4602c221
Shahbaz Ansari <shahbaz4783@gmail.com>
2024-10-25 21:15:59 +0530

    improve game controls button

--- src/components/gameplay/game-controls.tsx
+++ src/components/gameplay/game-controls.tsx
@@ -8,12 +8,14 @@ import {
 } from "@/src/lib/game-logics";
 import ShinyButton from "../magicui/shiny-button";
 import { useCricketGameState } from "@/src/lib/store";
-import { GameParticipant } from "@/src/types/gameState";
+import {
+  BattingStyle,
+  BowlingType,
+  GameParticipant,
+} from "@/src/types/gameState";
 
 // Define types for the game actions
-type BattingAction = "aggressive" | "normal" | "defensive";
-type BowlingAction = "normal" | "yorker" | "bouncer";
-type GameAction = BattingAction | BowlingAction;
+type GameAction = BattingStyle | BowlingType;
 
 export function GameControls() {
   const [disableControls, setDisableControls] = useState(false);
@@ -23,10 +25,10 @@ export function GameControls() {
     setDisableControls(true);
     const isBatting = gameState.gamePhase === "batting";
     const runsScored = isBatting
-      ? calculateRunsScored(action as BattingAction, computerBowling(gameState))
+      ? calculateRunsScored(action as BattingStyle, computerBowling(gameState))
       : calculateRunsScored(
-          getOpponentBattingStrategy(gameState, action as BowlingAction),
-          action as BowlingAction,
+          getOpponentBattingStrategy(gameState, action as BowlingType),
+          action as BowlingType,
         );
 
     const battingTeam: GameParticipant = isBatting ? "player" : "opponent";
@@ -50,12 +52,12 @@ export function GameControls() {
     </>
   );
 
-  const battingActions: BattingAction[] = ["aggressive", "normal", "defensive"];
-  const bowlingActions: BowlingAction[] = ["normal", "bouncer", "yorker"];
-  const colors = ["bg-red-300", "bg-green-300", "bg-slate-300"];
+  const battingActions: BattingStyle[] = ["aggressive", "normal", "defensive"];
+  const bowlingActions: BowlingType[] = ["normal", "bouncer", "yorker"];
+  const colors = ["bg-indigo-500", "bg-emerald-500", "bg-amber-500"];
 
   return (
-    <div className="grid p-4 sticky bottom-0 grid-cols-3 gap-4 w-full bg-slate-800/50 backdrop-blur-md rounded-xl">
+    <div className="grid p-2 sticky bottom-0 grid-cols-3 gap-4 w-full bg-slate-800/50 backdrop-blur-md rounded-xl">
       {gameState.gamePhase === "batting"
         ? renderButtons(battingActions, colors)
         : renderButtons(bowlingActions, colors)}
