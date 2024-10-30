dd58ef4915f2a565a57987ed00257dfc0d30ded1
Shahbaz Ansari <shahbaz4783@gmail.com>
2024-10-30 17:25:19 +0530

    fix avatar dialog

--- src/components/dialog/avatar-dialog.tsx
+++ src/components/dialog/avatar-dialog.tsx
@@ -3,101 +3,77 @@
 import { ChevronLeft, ChevronRight, X } from "lucide-react";
 import Image from "next/image";
 import {
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from "@/src/components/ui/dialog";
-import { Button } from "@/src/components/ui/button";
-import { token } from "@/src/lib/constants";
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+} from '@/src/components/ui/dialog';
+import { token } from '@/src/lib/constants';
+import { avatars } from '@/src/constants/avatars';
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { User } from 'lucide-react';
+import { Dialog, DialogTrigger } from '@/src/components/ui/dialog';
+import {
+	Avatar,
+	AvatarImage,
+	AvatarFallback,
+} from '@/src/components/ui/avatar';
+import { Button } from '@/src/components/ui/button';
+import { SubmitButton } from '../feedback/submit-button';
 
-const avatars = [
-	{
-		id: 1,
-		name: 'Classic',
-		free: true,
-		url: 'https://img.freepik.com/free-photo/portrait-man-cartoon-style_23-2151133925.jpg?t=st=1730133153~exp=1730136753~hmac=b1e262ec717ab51224779ed81a7930ae1fd6644abbc449bd233f1890c6cdb310&w=2000',
-	},
-	{
-		id: 2,
-		name: 'Cyber Batsman',
-		free: true,
-		url: 'https://img.freepik.com/free-photo/portrait-man-cartoon-style_23-2151133925.jpg?t=st=1730133153~exp=1730136753~hmac=b1e262ec717ab51224779ed81a7930ae1fd6644abbc449bd233f1890c6cdb310&w=2000',
-	},
-	{
-		id: 3,
-		name: 'Neon Bowler',
-		free: false,
-		price: 100,
-		url: 'https://img.freepik.com/free-photo/portrait-man-cartoon-style_23-2151133925.jpg?t=st=1730133153~exp=1730136753~hmac=b1e262ec717ab51224779ed81a7930ae1fd6644abbc449bd233f1890c6cdb310&w=2000',
-	},
-	{
-		id: 4,
-		name: 'Holographic Captain',
-		free: false,
-		price: 200,
-		url: 'https://img.freepik.com/free-photo/portrait-man-cartoon-style_23-2151133925.jpg?t=st=1730133153~exp=1730136753~hmac=b1e262ec717ab51224779ed81a7930ae1fd6644abbc449bd233f1890c6cdb310&w=2000',
-	},
-];
+export function AvatarDialog() {
+	const [selectedAvatar, setSelectedAvatar] = useState(avatars[0]);
+	const [isOpen, setIsOpen] = useState(false);
 
-export function AvatarDialog({
-  currentAvatar,
-  setCurrentAvatar,
-  onClose,
-}: {
-  currentAvatar: number;
-  setCurrentAvatar: any;
-  onClose: any;
-}) {
-  const handleAvatarChange = (direction: "prev" | "next") => {
-    setCurrentAvatar((prev: number) => {
-      if (direction === "prev") {
-        return prev > 1 ? prev - 1 : avatars.length;
-      } else {
-        return prev < avatars.length ? prev + 1 : 1;
-      }
-    });
-  };
+	return (
+		<Dialog open={isOpen} onOpenChange={setIsOpen}>
+			<DialogTrigger asChild>
+				<Button variant='ghost' className='relative h-16 w-16 rounded-full'>
+					<Avatar className='h-16 w-16 border-2 border-white'>
+						<AvatarImage src={selectedAvatar.href} alt={selectedAvatar.name} />
+						<AvatarFallback>
+							<User className='h-8 w-8' />
+						</AvatarFallback>
+					</Avatar>
+				</Button>
+			</DialogTrigger>
+			<DialogContent className='m-auto w-11/12 rounded-xl'>
+				<DialogHeader>
+					<DialogTitle>Choose your avatar</DialogTitle>
+				</DialogHeader>
+				<div className='grid grid-cols-2 gap-4 py-4'>
+					<AnimatePresence>
+						{avatars.map((avatar) => (
+							<motion.div
+								key={avatar.id}
+								initial={{ opacity: 0, scale: 0.8 }}
+								animate={{ opacity: 1, scale: 1 }}
+								exit={{ opacity: 0, scale: 0.8 }}
+								transition={{ duration: 0.2 }}
+								whileTap={{ scale: 0.9 }}
+							>
+								<Button
+									variant='outline'
+									className={`w-full h-24 p-0 rounded-xl flex flex-col ${
+										selectedAvatar.id === avatar.id ? 'ring-2 ring-primary' : ''
+									}`}
+									onClick={() => setSelectedAvatar(avatar)}
+								>
+									<Avatar className=''>
+										<AvatarImage src={avatar.href} alt={avatar.name} />
+										<AvatarFallback>
+											<User className='h-8 w-8' />
+										</AvatarFallback>
+									</Avatar>
+									<p className='mt-2 text-xs'>{avatar.name}</p>
+								</Button>
+							</motion.div>
+						))}
+					</AnimatePresence>
+				</div>
 
-  return (
-    <DialogContent className="sm:max-w-[425px]">
-      <DialogHeader>
-        <DialogTitle>Select Avatar</DialogTitle>
-      </DialogHeader>
-      <div className="flex items-center justify-between mb-4">
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => handleAvatarChange("prev")}
-        >
-          <ChevronLeft className="h-4 w-4" />
-        </Button>
-        <div className="relative">
-          <Image
-            src={avatars[currentAvatar - 1].url}
-            alt={avatars[currentAvatar - 1].name}
-            height={80}
-            width={80}
-            className="w-24 h-24 rounded-full"
-          />
-          {!avatars[currentAvatar - 1].free && (
-            <div className="absolute top-0 right-0 bg-yellow-500 text-black text-xs font-bold px-2 py-1 rounded-full">
-              {avatars[currentAvatar - 1].price} {token.symbol}
-            </div>
-          )}
-        </div>
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => handleAvatarChange("next")}
-        >
-          <ChevronRight className="h-4 w-4" />
-        </Button>
-      </div>
-      <p className="text-center mb-4">{avatars[currentAvatar - 1].name}</p>
-
-      <Button className="w-full rounded-xl" variant="default">
-        Choose Avatar
-      </Button>
-    </DialogContent>
-  );
+				<SubmitButton title='Choose' loadingTitle='Applty' />
+			</DialogContent>
+		</Dialog>
+	);
 }
--- src/components/home/user-profile-header.tsx
+++ src/components/home/user-profile-header.tsx
@@ -1,176 +1,133 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { motion } from "framer-motion";
-import { Zap } from "lucide-react";
-import { useGetUserInfo } from "@/src/hooks/useUserData";
-import { useInitData } from "@telegram-apps/sdk-react";
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/src/components/ui/avatar";
-import { Dialog, DialogTrigger } from "@/src/components/ui/dialog";
-import { Progress } from "@/src/components/ui/progress";
-import { token } from "@/src/lib/constants";
-import { AvatarDialog } from "../dialog/avatar-dialog";
-import { useUserAvatar } from "@/src/hooks/useUserAvatar";
-import { Card } from "@/src/components/ui/card";
-import { saveOrUpdateUser } from "@/src/actions/user.action";
-import { Skeleton } from "../ui/skeleton";
+import { useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { useGetUserInfo } from '@/src/hooks/useUserData';
+import { useInitData } from '@telegram-apps/sdk-react';
+import { Progress } from '@/src/components/ui/progress';
+import { token } from '@/src/lib/constants';
+import { AvatarDialog } from '../dialog/avatar-dialog';
+import { Card } from '@/src/components/ui/card';
+import { saveOrUpdateUser } from '@/src/actions/user.action';
+import { Skeleton } from '../ui/skeleton';
 
 interface UserXP {
-  level: number;
-  totalXP: number;
-  xpForNextLevel: number;
-  levelName: string;
+	level: number;
+	totalXP: number;
+	xpForNextLevel: number;
+	levelName: string;
 }
 
 interface WalletInfo {
-  balance: number;
+	balance: number;
 }
 
 interface UserData {
-  userXP: UserXP;
-  walletInfo: WalletInfo;
-}
-
-interface UserAvatarProps {
-  currentAvatar: number;
-  setCurrentAvatar: (avatar: number) => void;
-  userName: string | undefined;
-  levelName: string | undefined;
-  isLoading: boolean;
+	userXP: UserXP;
+	walletInfo: WalletInfo;
 }
 
 interface UserStatsProps {
-  level: number;
-  balance: number;
-  isLoading: boolean;
+	level: number;
+	balance: number;
+	isLoading: boolean;
+	levelName: string | undefined;
+	name: string | undefined;
 }
 
 interface XPProgressProps {
-  totalXP: number;
-  xpForLevelUp: number;
-  xpForNextLevel: number;
-  isLoading: boolean;
+	totalXP: number;
+	xpForLevelUp: number;
+	xpForNextLevel: number;
+	isLoading: boolean;
 }
 
 export function UserProfileHeader(): JSX.Element {
-  const initData = useInitData();
-  const user = initData?.user;
+	const initData = useInitData();
+	const user = initData?.user;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (!user) return;
-        await saveOrUpdateUser(user);
-      } catch (error) {
-        console.error("Error saving/updating user data:", error);
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				if (!user) return;
+				await saveOrUpdateUser(user);
+			} catch (error) {
+				console.error('Error saving/updating user data:', error);
+			}
+		};
+		fetchData();
+	}, [user]);
 
-    fetchData();
-  }, [user]);
+	const { data, isLoading } = useGetUserInfo(user?.id);
+	const userData: UserData = data as UserData;
 
-  const { data, isLoading } = useGetUserInfo(user?.id);
-  const { currentAvatar, setCurrentAvatar } = useUserAvatar(user?.id);
+	const level = userData?.userXP.level;
+	const totalXP = userData?.userXP.totalXP;
+	const balance = userData?.walletInfo.balance;
+	const xpForLevelUp = userData?.userXP.xpForNextLevel;
+	const xpForNextLevel = xpForLevelUp - totalXP;
 
-  const userData: UserData = data as UserData;
+	return (
+		<Card className='rounded-xl p-4 space-y-5 bg-gradient-to-r from-gray-800 to-gray-900 shadow-lg'>
+			<motion.div
+				className='flex gap-2 items-center justify-between'
+				initial={{ opacity: 0, y: -20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5 }}
+			>
+				<AvatarDialog />
+				<UserStats
+					level={level}
+					balance={balance}
+					isLoading={isLoading}
+					name={user?.firstName}
+					levelName={userData?.userXP?.levelName}
+				/>
+			</motion.div>
 
-  const level = userData?.userXP.level;
-  const totalXP = userData?.userXP.totalXP;
-  const balance = userData?.walletInfo.balance;
-  const xpForLevelUp = userData?.userXP.xpForNextLevel;
-  const xpForNextLevel = xpForLevelUp - totalXP;
-
-  return (
-    <Card className="rounded-xl p-4 space-y-5 bg-gradient-to-r from-gray-800 to-gray-900 shadow-lg">
-      <motion.div
-        className="flex items-center justify-between"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <UserAvatar
-          currentAvatar={currentAvatar}
-          setCurrentAvatar={setCurrentAvatar}
-          userName={user?.firstName}
-          levelName={userData?.userXP?.levelName}
-          isLoading={isLoading}
-        />
-        <UserStats level={level} balance={balance} isLoading={isLoading} />
-      </motion.div>
-
-      <XPProgress
-        totalXP={totalXP}
-        xpForLevelUp={xpForLevelUp}
-        xpForNextLevel={xpForNextLevel}
-        isLoading={isLoading}
-      />
-    </Card>
-  );
+			<XPProgress
+				totalXP={totalXP}
+				xpForLevelUp={xpForLevelUp}
+				xpForNextLevel={xpForNextLevel}
+				isLoading={isLoading}
+			/>
+		</Card>
+	);
 }
 
-function UserAvatar({
-  currentAvatar,
-  setCurrentAvatar,
-  userName,
-  levelName,
-  isLoading,
-}: UserAvatarProps): JSX.Element {
-  return (
-		<div className='flex items-center space-x-3'>
-			<div className='relative'>
-				<Dialog>
-					<DialogTrigger asChild>
-						<Avatar className='h-14 w-14 ring-2 ring-primary cursor-pointer'>
-							<AvatarImage
-								src={`https://res.cloudinary.com/dw2o2w9zg/image/upload/v1730138783/9200_3_2_03_tv5m8n.jpg`}
-								alt='User Avatar'
-							/>
-							<AvatarFallback>{userName?.charAt(0) ?? 'U'}</AvatarFallback>
-						</Avatar>
-					</DialogTrigger>
-					<AvatarDialog
-						currentAvatar={currentAvatar}
-						setCurrentAvatar={setCurrentAvatar}
-						onClose={() => {}}
-					/>
-				</Dialog>
-				<div className='absolute -bottom-1 -right-1 bg-primary rounded-full p-1'>
-					<Zap className='h-4 w-4 text-yellow-300' />
-				</div>
-			</div>
+function UserStats({
+	level,
+	balance,
+	name,
+	levelName,
+	isLoading,
+}: UserStatsProps) {
+	return (
+		<section className='border flex justify-between w-full'>
 			<div>
-				<h2 className='text-xl font-bold'>{userName ?? 'User'}</h2>
+				<h2 className='text-xl font-bold'>{name ?? 'Guest Users'}</h2>
 				{isLoading ? (
 					<Skeleton className='h-2 mt-1 w-20 bg-slate-200 bg-opacity-20 rounded-xl' />
 				) : (
 					<span className='text-sm text-yellow-300'>{levelName}</span>
 				)}
 			</div>
-		</div>
+			<div className='text-right flex flex-col items-end'>
+				{isLoading ? (
+					<Skeleton className='h-3 w-16 bg-slate-200 bg-opacity-20 rounded-xl' />
+				) : (
+					<p className='text-sm text-yellow-300'>Level {level}</p>
+				)}
+				{isLoading ? (
+					<Skeleton className='h-3 mt-2 w-20 bg-slate-200 bg-opacity-20 rounded-xl' />
+				) : (
+					<p className='text-xl font-bold'>
+						{balance} <span className='text-sm'>{token.symbol}</span>
+					</p>
+				)}
+			</div>
+		</section>
 	);
-}
-
-function UserStats({ level, balance, isLoading }: UserStatsProps): JSX.Element {
-  return (
-    <div className="text-right flex flex-col items-end">
-      {isLoading ? (
-        <Skeleton className="h-3 w-16 bg-slate-200 bg-opacity-20 rounded-xl" />
-      ) : (
-        <p className="text-sm text-yellow-300">Level {level}</p>
-      )}
-      {isLoading ? (
-        <Skeleton className="h-3 mt-2 w-20 bg-slate-200 bg-opacity-20 rounded-xl" />
-      ) : (
-        <p className="text-xl font-bold">
-          {balance} <span className="text-sm">{token.symbol}</span>
-        </p>
-      )}
-    </div>
-  );
 }
 
 function XPProgress({
