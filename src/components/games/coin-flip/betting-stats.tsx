04b2accb5e64887973fc8f30c1781578e50b2d49
Shahbaz Ansari <shahbaz4783@gmail.com>
2024-11-15 17:12:59 +0530

    improve bet stats ui

--- src/components/games/coin-flip/betting-stats.tsx
+++ src/components/games/coin-flip/betting-stats.tsx
@@ -4,9 +4,14 @@ import { useGetUserBettingStats } from '@/src/hooks/useUserData';
 import { useInitData } from '@telegram-apps/sdk-react';
 import { StatCard } from '../../common/cards/stats-card';
 import { motion } from 'framer-motion';
-import { useState } from 'react';
-import { Button } from '@/src/components/ui/button';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { Card, CardContent, CardTitle } from '@/src/components/ui/card';
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from '@/src/components/ui/carousel';
 
 const betTypes = [
 	'SAFE_BET',
@@ -19,39 +24,15 @@ export function BettingStats() {
 	const initData = useInitData();
 	const user = initData?.user;
 
-	const safeBetStats = useGetUserBettingStats(user?.id, 'SAFE_BET').data;
-	const classicFlipStats = useGetUserBettingStats(
-		user?.id,
-		'CLASSIC_FLIP'
-	).data;
-	const tripleShotStats = useGetUserBettingStats(user?.id, 'TRIPLE_SHOT').data;
-	const jackpotStats = useGetUserBettingStats(user?.id, 'JACKPOT').data;
-
 	const bettingStats = {
-		SAFE_BET: safeBetStats,
-		CLASSIC_FLIP: classicFlipStats,
-		TRIPLE_SHOT: tripleShotStats,
-		JACKPOT: jackpotStats,
+		SAFE_BET: useGetUserBettingStats(user?.id, 'SAFE_BET').data,
+		CLASSIC_FLIP: useGetUserBettingStats(user?.id, 'CLASSIC_FLIP').data,
+		TRIPLE_SHOT: useGetUserBettingStats(user?.id, 'TRIPLE_SHOT').data,
+		JACKPOT: useGetUserBettingStats(user?.id, 'JACKPOT').data,
 	};
-
-	const [selectedBetTypeIndex, setSelectedBetTypeIndex] = useState(0);
-
-	const handlePrevious = () => {
-		setSelectedBetTypeIndex((prev) =>
-			prev === 0 ? betTypes.length - 1 : prev - 1
-		);
-	};
-
-	const handleNext = () => {
-		setSelectedBetTypeIndex((prev) =>
-			prev === betTypes.length - 1 ? 0 : prev + 1
-		);
-	};
-
-	const selectedBetType = betTypes[selectedBetTypeIndex];
 
 	const renderBettingStatContent = (data: any) => (
-		<div className='grid grid-cols-2 gap-3'>
+		<div className='grid grid-cols-2 gap-2'>
 			<StatCard title='Total Bets' value={data?.betsPlaced ?? 0} />
 			<StatCard
 				title='Bets Won'
@@ -86,41 +67,28 @@ export function BettingStats() {
 			transition={{ duration: 0.5 }}
 			className='space-y-6 bg-slate-900 rounded-xl p-4 backdrop-blur-md relative text-center'
 		>
-			<h2 className='text-2xl font-semibold text-white tracking-wide mb-6 bg-gradient-to-r from-purple-500 to-indigo-500 p-3 rounded-xl'>
+			<h2 className='text-xl font-semibold text-white tracking-wide mb-6 bg-gradient-to-r from-sky-900 to-slate-800 p-3 rounded-xl'>
 				Your Betting Performance
 			</h2>
-			<div className='flex justify-between items-center mb-6'>
-				<Button
-					variant='ghost'
-					size='icon'
-					onClick={handlePrevious}
-					className='text-white hover:bg-slate-800'
-				>
-					<ChevronLeft className='h-6 w-6' />
-				</Button>
-				<span className='text-lg font-medium text-white'>
-					{selectedBetType.replace(/_/g, ' ')}
-				</span>
-				<Button
-					variant='ghost'
-					size='icon'
-					onClick={handleNext}
-					className='text-white hover:bg-slate-800'
-				>
-					<ChevronRight className='h-6 w-6' />
-				</Button>
-			</div>
 
-			<motion.div
-				key={selectedBetType}
-				initial={{ opacity: 0, x: 50 }}
-				animate={{ opacity: 1, x: 0 }}
-				exit={{ opacity: 0, x: -50 }}
-				transition={{ duration: 0.3 }}
-				className='flex justify-center items-center mt-6'
-			>
-				{renderBettingStatContent(bettingStats[selectedBetType])}
-			</motion.div>
+			<Carousel className='w-4/5 m-auto'>
+				<CarouselContent>
+					{betTypes.map((betType, index) => (
+						<CarouselItem key={index}>
+							<Card className='rounded-xl'>
+								<CardTitle className='pt-4'>
+									{betType.replace(/_/g, ' ')}
+								</CardTitle>
+								<CardContent className='p-4'>
+									{renderBettingStatContent(bettingStats[betType])}
+								</CardContent>
+							</Card>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
 		</motion.section>
 	);
 }
